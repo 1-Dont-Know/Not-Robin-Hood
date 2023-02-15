c8edc519819bc859f998309448954d741c86d30d
Aveneil Lucena <neillucena97@gmail.com>
2023-02-15 14:45:22 -0800

    Stock viewer page routing

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,6 @@
-import { SIGNUP_ROUTE } from "../utils/consts";
+import { SIGNUP_ROUTE, STOCK_VIEWER_ROUTE } from "../utils/consts";
 import Signup from "../components/pages/Signup/Signup";
+import Stock_Viewer from "../components/pages/Stock_Viewer/Stock_Viewer";
 
 // public routes
 
@@ -7,6 +8,10 @@ export const publicRoutes = [
   {
     path: SIGNUP_ROUTE,
     element: Signup,
+  },
+  {
+    path: STOCK_VIEWER_ROUTE,
+    element: Stock_Viewer,
   },
 ];
 
--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,3 +1,4 @@
 export const LOGIN_ROUTE = "/login";
 export const SIGNUP_ROUTE = "/signup";
 export const PORTFOLIO_ROUTE = "/portfolio";
+export const STOCK_VIEWER_ROUTE = "/stock_viewer";

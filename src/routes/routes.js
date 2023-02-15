8d973cbe9c6bd0e3360931197a3fd1646c12bd0c
IDK <61032214+1-Dont-Know@users.noreply.github.com>
2023-02-15 14:29:43 -0800

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,4 @@
-import { LOGIN_ROUTE, PORTFOLIO_ROUTE, SIGNUP_ROUTE, Account_ROUTE, MARKETS_PAGE} from "../utils/consts";
+import { LOGIN_ROUTE, PORTFOLIO_ROUTE, SIGNUP_ROUTE, ACCOUNT_ROUTE, MARKETS_PAGE} from "../utils/consts";
 import Signup from "../components/pages/Signup/Signup";
 import Portfolio from "../components/pages/Portfolio/Portfolio";
 import Login from "../components/pages/Login/Login";
@@ -12,18 +12,21 @@ export const publicRoutes = [
     path: SIGNUP_ROUTE,
     element: Signup,
   },
+  
   {
     Markets-page
     path: MARKETS_PAGE,
     element: Markets,
-  }
- 
+  },
+  
+  {
     path: LOGIN_ROUTE,
     element: Login,
   },
+  
   {
 
-    path: Account_ROUTE,
+    path: ACCOUNT_ROUTE,
     element: Account,
   },
   

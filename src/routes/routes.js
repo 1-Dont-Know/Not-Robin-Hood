a708a1b4c67e73470d7004f62003abda564e2240
CuylerR <Cuyler10@gmail.com>
2023-02-15 15:23:33 -0800

    Setting-Route-Path

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,11 @@
-import { LOGIN_ROUTE, PORTFOLIO_ROUTE, SIGNUP_ROUTE, STOCK_VIEWER_ROUTE, ACCOUNT_ROUTE, MARKETS_PAGE} from "../utils/consts";
+import { LOGIN_ROUTE, PORTFOLIO_ROUTE, SIGNUP_ROUTE, STOCK_VIEWER_ROUTE, ACCOUNT_ROUTE, MARKETS_PAGE, SETTINGS_ROUTE} from "../utils/consts";
 import Signup from "../components/pages/Signup/Signup";
 import Portfolio from "../components/pages/Portfolio/Portfolio";
 import Login from "../components/pages/Login/Login";
 import Account from "../components/pages/Account/Account";
 import Markets from "../components/pages/Markets/Markets";
 import Stock_Viewer from "../components/pages/Stock_Viewer/Stock_Viewer";
-
+import Settings from "../components/pages/Settings/Settings";
 //? PUBLIC ROUTES
 
 export const publicRoutes = [
@@ -34,6 +34,10 @@ export const publicRoutes = [
     element: Account,
   },
   
+  {
+    path: SETTINGS_ROUTE,
+    element: Settings,
+  }
 ];
 
 //? PRIVATE ROUTES (routes which are available for authorized users only)
--- src/utils/consts.js
+++ src/utils/consts.js
@@ -6,3 +6,4 @@ export const PORTFOLIO_ROUTE = "/portfolio";
 export const STOCK_VIEWER_ROUTE = "/stock_viewer";
 export const ACCOUNT_ROUTE = "/account";
 export const MARKETS_PAGE = "/markets";
+export const SETTINGS_ROUTE ="/setting";

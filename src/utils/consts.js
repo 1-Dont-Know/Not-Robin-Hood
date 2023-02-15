4a7d198e8fd80ba4a6f5f3278679921e77755afb
IDK <61032214+1-Dont-Know@users.noreply.github.com>
2023-02-15 14:30:07 -0800

    Update consts.js

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -4,5 +4,5 @@ export const LOGIN_ROUTE = "/login";
 export const SIGNUP_ROUTE = "/signup";
 export const PORTFOLIO_ROUTE = "/portfolio";
 
-export const Account_ROUTE = "/account";
+export const ACCOUNT_ROUTE = "/account";
 export const MARKETS_PAGE = "/markets";

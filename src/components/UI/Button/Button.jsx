ca2a6f192afc37762bc3b3b56159639b1cc482fb
rootfellen <rootfellenua@gmail.com>
2023-02-22 15:31:28 -0800

    add font fix on signup/login page

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -21,10 +21,8 @@ const Button = ({ type, children }) => {
           ? styles.profileButton
           : type === "tools"
           ? styles.toolsButton
-
           : type === "timeBar"
           ? styles.timeBarButton
-
           : type === "settings"
           ? styles.settingsButton
           : type === "buyBox"

0f36d47ef1be9754e280c6649bc0cc634c400215
rootfellen <rootfellenua@gmail.com>
2023-02-22 20:20:27 -0800

    add nav links,fix topnav

--- src/components/UI/SidebarNav/SidebarNav.jsx
+++ src/components/UI/SidebarNav/SidebarNav.jsx
@@ -5,6 +5,7 @@ import markets from "../../../assets/icons/markets.svg";
 import transactions from "../../../assets/icons/transactions.svg";
 import settings from "../../../assets/icons/settings.svg";
 import logout from "../../../assets/icons/logout.svg";
+import { NavLink } from "react-router-dom";
 
 const SidebarNav = () => {
   return (
@@ -16,19 +17,29 @@ const SidebarNav = () => {
             <hr className={styles.divider} />
           </span>
         </h2>
-        <li className={styles.listItem}>
-          <img src={overview} alt="overview" />
-          Overview
-        </li>
-        <li className={styles.listItem}>
-          {" "}
-          <img src={markets} alt="markets" />
-          Markets
-        </li>
-        <li className={styles.listItem}>
-          {" "}
-          <img src={transactions} alt="transactions" /> Stock Transaction
-        </li>
+        <NavLink to="/account">
+          {({ isActive }) => (
+            <li className={isActive ? styles.active : styles.listItem}>
+              <img src={overview} alt="overview" />
+              Overview
+            </li>
+          )}
+        </NavLink>
+        <NavLink to="/markets">
+          {({ isActive }) => (
+            <li className={isActive ? styles.active : styles.listItem}>
+              <img src={markets} alt="markets" />
+              Markets
+            </li>
+          )}
+        </NavLink>
+        <NavLink to="/stock-transactions">
+          {({ isActive }) => (
+            <li className={isActive ? styles.active : styles.listItem}>
+              <img src={transactions} alt="transactions" /> Stock Transaction
+            </li>
+          )}
+        </NavLink>
       </ul>
       <ul className={styles.supportList}>
         <h2 className={styles.sectionTitle}>
@@ -37,14 +48,18 @@ const SidebarNav = () => {
             <hr className={styles.divider} />
           </span>
         </h2>
-        <li className={styles.listItem}>
-          <img src={settings} alt="settings" />
-          Settings
-        </li>
-        <li className={styles.listItem}>
-          <img src={logout} alt="logout" />
-          Log out
-        </li>
+        <NavLink to="/settings">
+          <li className={styles.listItem}>
+            <img src={settings} alt="settings" />
+            Settings
+          </li>
+        </NavLink>
+        <NavLink to="/signup">
+          <li className={styles.listItem} id="logout">
+            <img src={logout} alt="logout" />
+            Log out
+          </li>
+        </NavLink>
       </ul>
     </nav>
   );
--- src/components/pages/StockTransactions/StockTransactions.jsx
+++ src/components/pages/StockTransactions/StockTransactions.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from "./StockTransactions.module.scss";
 import Search from "../../UI/Search/Search";
+import Sidebar from "../../UI/Sidebar/Sidebar";
 
 const StockTransactions = () => {
-    return (
-        <div>
-            <Search placeholder="Search for Market" />
-        </div>
-    );
-}
+  return (
+    <div>
+      {/* <Search placeholder="Search for Market" /> */}
+      <Sidebar />
+    </div>
+  );
+};
 
 export default StockTransactions;

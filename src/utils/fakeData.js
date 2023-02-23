72acae75eea5c66f19ab6e5e5412a637de4c24cb
David Bergstrom <davidmbergstrom@gmail.com>
2023-02-23 15:24:15 -0800

    Added more fake data for testing

--- src/utils/fakeData.js
+++ src/utils/fakeData.js
@@ -2,7 +2,7 @@ export const fakeData = [
     {
         id: 1,
         day: 1,
-        price: 500,
+        price: 130,
     },
     {
         id: 2,
@@ -13,6 +13,31 @@ export const fakeData = [
         id: 3,
         day: 3,
         price: 150,
+    },
+    {
+        id: 4,
+        day: 4,
+        price: 300,
+    },
+    {
+        id: 5,
+        day: 5,
+        price: 330,
+    },
+    {
+        id: 6,
+        day: 6,
+        price: 360,
+    },
+    {
+        id: 7,
+        day: 7,
+        price: 359,
+    },
+    {
+        id: 8,
+        day: 8,
+        price: 371,
     },
 
 ];

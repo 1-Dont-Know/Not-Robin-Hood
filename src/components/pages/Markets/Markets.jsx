ecbdcc0e0338869ed942a864a3f2a3d5b7cac159
rootfellen <rootfellenua@gmail.com>
2023-02-23 16:12:42 -0800

    refactor hero component, add topnav and sidebar

--- src/components/pages/Markets/Markets.jsx
+++ src/components/pages/Markets/Markets.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Sidebar from "../../UI/Sidebar/Sidebar";
+import TopNav from "../../UI/TopNav/TopNav";
+import styles from "./Markets.module.scss";
+import Hero from "../../UI/Hero/Hero";
 
 export default function Markets() {
   return (
     <>
-      <Sidebar />
+      <div className={styles.wrapper}>
+        {/* Sidebar Section */}
+        <Sidebar />
+        {/* Nav/Hero Section */}
+        <section className={styles.heroSection}>
+          {/* Top Navigation */}
+          <TopNav />
+          {/* Hero Section */}
+          <Hero />
+        </section>
+      </div>
     </>
   );
 }

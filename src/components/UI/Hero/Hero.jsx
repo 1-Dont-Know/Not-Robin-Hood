f49843fe8cf381566fb7d69c9eb57719af25ec5c
rootfellen <rootfellenua@gmail.com>
2023-02-24 16:13:43 -0800

    add hero, graph changes

--- src/components/UI/Hero/Hero.jsx
+++ src/components/UI/Hero/Hero.jsx
@@ -3,34 +3,12 @@ import styles from "./Hero.module.scss";
 
 import FeaturedStock from "../FeaturedStock/FeaturedStock";
 import Graph from "../Graph/Graph";
-import { fakeData } from "../../../utils/fakeData" //Temporary Fake Data used for Testing
+import { fakeData } from "../../../utils/fakeData"; //Temporary Fake Data used for Testing
 
-const Hero = () => {
-
+const Hero = ({ children }) => {
   //State Hook for Graph Component
-  const [stockData, setStockData] = useState({
-    labels: fakeData.map((data) => data.day),
-    datasets: [{
-      label: "Price",
-      data: fakeData.map((data) => data.price),
-    }],
-  });
 
-
-  return (
-    <section className={styles.container}>
-      <div className={styles.featured}>
-        <FeaturedStock status="up" />
-        <FeaturedStock status="down" />
-        <FeaturedStock status="up" />
-        <FeaturedStock status="up" />
-      </div>
-
-      {/* Graph Component */}
-      <Graph chartData={stockData} />
-
-    </section>
-  );
+  return <>{children}</>;
 };
 
 export default Hero;

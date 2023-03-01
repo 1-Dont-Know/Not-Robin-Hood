df6ed7cc8fa0e2bbd46fe3e9947afa500d0c1203
David Bergstrom <davidmbergstrom@gmail.com>
2023-03-01 15:17:26 -0800

    Worked on Stock-Viewer Responsiveness

--- src/components/pages/StockViewer/StockViewer.jsx
+++ src/components/pages/StockViewer/StockViewer.jsx
@@ -43,9 +43,9 @@ const AppFundsPopup = () => {
          {/* <AddFunds></AddFunds> */}
           <div className={styles.preview}>
            {/* //! GRAPH SECTION */}
-            <section className={styles.graph}>
+            {/* <section className={styles.graph}> */}
               <Graph chartData={stockData} />
-            </section>
+            {/* </section> */}
             <section className={styles.buybox}>
             <BuyBox/>
             </section>
@@ -60,5 +60,7 @@ const AppFundsPopup = () => {
 
   );
 };
+
+
 
 export default AppFundsPopup;

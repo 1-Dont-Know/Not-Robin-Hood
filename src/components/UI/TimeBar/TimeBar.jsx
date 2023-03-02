6b8d0674edae787dd9e8d70c3876c926afc96ccf
rootfellen <rootfellenua@gmail.com>
2023-03-02 13:18:04 -0800

    add refactor for button, remove flexbox mixin

--- src/components/UI/TimeBar/TimeBar.jsx
+++ src/components/UI/TimeBar/TimeBar.jsx
@@ -1,38 +1,38 @@
 import React from "react";
-import Button from "../Button/Button";
 import styles from "./TimeBar.module.scss";
+import globalStyles from "../../../styles/main.module.scss";
 
 const TimeBar = () => {
   return (
     <>
       <div className={styles.timeBar}>
-        <Button type="timeBar">
+        <button className={globalStyles.timeBarButton}>
           <div className={styles.timeBarButton}>1d</div>
-        </Button>
-        <Button type="timeBar">
+        </button>
+        <button className={globalStyles.timeBarButton}>
           <div className={styles.timeBarButton}>5d</div>
-        </Button>
-        <Button type="timeBar">
+        </button>
+        <button className={globalStyles.timeBarButton}>
           <div className={styles.timeBarButton}>2w</div>
-        </Button>
-        <Button type="timeBar">
+        </button>
+        <button className={globalStyles.timeBarButton}>
           <div className={styles.timeBarButton}>1m</div>
-        </Button>
-        <Button type="timeBar">
+        </button>
+        <button className={globalStyles.timeBarButton}>
           <div className={styles.timeBarButton}>6m</div>
-        </Button>
-        <Button type="timeBar">
+        </button>
+        <button className={globalStyles.timeBarButton}>
           <div className={styles.timeBarButton}>1y</div>
-        </Button>
-        <Button type="timeBar">
+        </button>
+        <button className={globalStyles.timeBarButton}>
           <div className={styles.timeBarButton}>5y</div>
-        </Button>
-        <Button type="timeBar">
+        </button>
+        <button className={globalStyles.timeBarButton}>
           <div className={styles.timeBarButton}>7y</div>
-        </Button>
-        <Button type="timeBar">
+        </button>
+        <button className={globalStyles.timeBarButton}>
           <div className={styles.timeBarButton}>MAX</div>
-        </Button>
+        </button>
       </div>
     </>
   );

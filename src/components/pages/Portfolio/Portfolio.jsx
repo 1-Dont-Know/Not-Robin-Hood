2a6fd80fe5b72dd6ae1b5634aa6eee1ce097d537
David Bergstrom <davidmbergstrom@gmail.com>
2023-03-02 14:34:53 -0800

    Updated Responsiveness

--- src/components/pages/Portfolio/Portfolio.jsx
+++ src/components/pages/Portfolio/Portfolio.jsx
@@ -3,6 +3,7 @@ import Sidebar from "../../UI/Sidebar/Sidebar";
 import TopNav from "../../UI/TopNav/TopNav";
 import styles from "./Portfolio.module.scss";
 import Button from "../../UI/Button/Button";
+import Hero from "../../UI/Hero/Hero";
 import vector from "../../../assets/icons/vector.svg";
 import {
   Chart as ChartJS,
@@ -23,10 +24,10 @@ const Portfolio = () => {
   //Fake data for pie chart
   const data = {
     labels: [],
-    datasets:[{
+    datasets: [{
       label: 'Portfolio Value',
       data: [45, 10, 45],
-      backgroundColor: ['#2AB795','#C18E60','#37433A',],
+      backgroundColor: ['#2AB795', '#C18E60', '#37433A',],
       borderColor: ['#2AB795', '#C18E60', '#37433A'],
     }]
   }
@@ -35,17 +36,17 @@ const Portfolio = () => {
 
   }
 
-  const textCenter ={
+  const textCenter = {
     id: "textCenter",
-    beforeDatasetsDraw(chart, args, pluginOptions){
-      const {ctx, data } = chart;
+    beforeDatasetsDraw(chart, args, pluginOptions) {
+      const { ctx, data } = chart;
       ctx.save();
       ctx.font = 'bolder 24px sans-serif';
       ctx.fillStyle = 'black';
       ctx.textAlign = 'center';
-      ctx.fillText(`Total Portfolio Value: $200`, 
-      chart.getDatasetMeta(0).data[0].x, 
-      chart.getDatasetMeta(0).data[0].y);
+      ctx.fillText(`Total Portfolio Value: $200`,
+        chart.getDatasetMeta(0).data[0].x,
+        chart.getDatasetMeta(0).data[0].y);
     }
   }
   return (
@@ -53,83 +54,71 @@ const Portfolio = () => {
       <div className={styles.wrapper}>
         {/* Sidebar Section */}
         <Sidebar />
-
         {/* Nav/Hero Section */}
-        <section className={styles.heroSection}>
+        <main className={styles.mainSection}>
           {/* Top Navigation */}
           <TopNav />
+          {/* Hero Section */}
+          <Hero>
+            <div className={styles.tanSquare}>
+              <div className="row">
+                <div className="details">
+                  <ol className={styles.valueText}>Total Portfolio Value</ol>
+                  <ol className={styles.dollarAmount}>$200.00</ol>
+                  <ol className={styles.stocksOptions}>
+                    Stocks & Options
+                    <span className={styles.wordSpace}>26.35%</span>
+                    <span className={styles.moneyColorGreen}>$52.7</span>
+                  </ol>
+                  <hr className={styles.line}></hr>
+                  <ol className={styles.stocksOptions}>
+                    Cryptocurrencies
+                    <span className={styles.wordSpace}>2.09%</span>
+                    <span className={styles.moneyColorTan}>$4.18</span>
+                  </ol>
+                  <hr className={styles.line}></hr>
+                  <ol className={styles.stocksOptions}>
+                    Brokerage Cash
+                    <span className={styles.wordSpace}> 71.65%</span>
+                    <span className={styles.moneyAmount}>$143.24</span>
+                  </ol>
+                  <hr className={styles.line}></hr>
+                </div>
 
-          <div className={styles.accName}>
-            {/* Under this section will be populated by the name given
-            in the account */}
-            <strong className={styles.name}>John Doe</strong>
-            
+                <div className={styles.doughnutGraph}>
+                  <Doughnut
+                    data={data}
+                    options={options}
+                    plugins={[textCenter]}
+                  ></Doughnut>
+                </div>
+              </div>
 
-            <div className={styles.ihsButtons}>
-              <Button type="settings">
-                <strong className={styles.ihs}>Investing</strong>
-              </Button>
 
-              <Button type="settings">
-                <strong className={styles.ihs}>History</strong>
-              </Button>
-
-              <Button type="settings">
-                <strong className={styles.ihs}>Settings</strong>
-              </Button>
-            </div>
-          </div>
-          
-          <div className={styles.tanSquare}>
-            <ol className={styles.valueText}>Total Portfolio Value</ol>
-            <ol className={styles.dollarAmount}>$200.00</ol>
-            <ol className={styles.stocksOptions}>
-            
-              {" "}
-              Stocks & Options
-              <span className={styles.wordSpace}>26.35%</span>
-              <span className={styles.moneyColorGreen}>$52.7</span>
-            </ol>
-            <hr className={styles.line}></hr>
-            <ol className={styles.stocksOptions}>
-              {" "}
-              Cryptocurrencies
-              <span className={styles.wordSpace}>2.09%</span>
-              <span className={styles.moneyColorTan}>$4.18</span>
-            </ol>
-            <hr className={styles.line}></hr>
-            <ol className={styles.stocksOptions}>
-              {" "}
-              Brokerage Cash
-              <span className={styles.wordSpace}> 71.65%</span>
-              <span className={styles.moneyAmount}>$143.24</span>
-            </ol>
-            <hr className={styles.line}></hr>
-            <div className={styles.doughnutGraph}>
-              <Doughnut
-                data = {data}
-                options = {options}
-                plugins = {[textCenter]}
-              ></Doughnut>
-            </div>
-
-              
-
-            <div className={styles.profileLowerSection}>
-              <span>
-                <Button type="settings">
-                  <span className={styles.pls}>
-                    Portfolio Lower Section{" "}
-                    <img src={vector} alt="vector"></img>
+              <div className="row">
+                <div className={styles.profileLowerSection}>
+                  <span>
+                    <Button type="settings">
+                      <span className={styles.pls}>
+                        Portfolio Lower Section{" "}
+                        <img src={vector} alt="vector"></img>
+                      </span>
+                    </Button>
                   </span>
-                </Button>
-              </span>
+                </div>
+              </div>
             </div>
-          </div>
-        </section>
+          </Hero>
+        </main>
       </div>
     </>
   );
 };
 
 export default Portfolio;
+
+
+
+
+
+

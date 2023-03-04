8bb489bdf8d5b7e15a27e97394127d8d52f2f825
Hoang Nguyen <91161497+Hnguyen209@users.noreply.github.com>
2023-03-03 16:06:19 -0800

    created a sort button so Markets.jsx

--- src/components/UI/StockItem/StockItem.jsx
+++ src/components/UI/StockItem/StockItem.jsx
@@ -1,69 +1,66 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./StockItem.module.scss";
 import globalStyles from "../../../styles/main.module.scss";
 import DownVectorIcon from "../../../assets/icons/down-vector.svg";
 import cart from "../../../assets/icons/shopping-cart.svg";
 
+// Accordion components that take in props as argument
+// manages its open/closed state with the isOpen state variable
 function Accordion(props) {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = React.useState(false);
 
+  // When the user clicks on the accordion, the toggleAccordion function is called to toggle the state of isOpen.
   const toggleAccordion = () => {
     setIsOpen(!isOpen);
   };
 
   return (
     <div className={styles.stockInfo}>
+      {/* added onClick where trigger function called toggleAccordion when clicked */}
       <button className={globalStyles.stockInfoButton} onClick={toggleAccordion}>
+        {/* props.title is used to display the title of the accordion, which is "Stock Information" in this case */}
         <p>{props.title}</p>
         <img src={DownVectorIcon} alt="arrow down" className={isOpen ? styles.rotated : ""}/>
       </button>
+      {/* Check if the accordion should be open and show the content if true */}
       {isOpen && (
         <div className={styles.stockInfoContent}>
+          {/* props.children is calling the <p> inside the accordion */}
           <div className={styles.stockInfoText}>{props.children}</div>
         </div>
       )}
     </div>
   );
 }
-/*fake data */
-const stockSymbols = ["AAPL", "GOOG", "TSLA", "AMZN", "FB"];
-const stockValues = [100, 200, 300, 400, 500];
 
-const generateRandomStockValue = () => {
-  const randomIndex = Math.floor(Math.random() * stockValues.length);
-  return stockValues[randomIndex];
-};
+const StockItem = ({ symbol, value }) => {
 
-
-const StockMarket = () => {
   return (
-    <>
-      <div>
-        <div className={styles.stockSection}>
-          <div className={styles.stockBody}>
-            
-            <button className={globalStyles.stockSymbolButton}>
-              {stockSymbols[Math.floor(Math.random() * stockSymbols.length)]}
-            </button>
+    <div>
+      <div className={styles.stockSection}>
+        <div className={styles.stockBody}>
 
-            <button className={globalStyles.stockValueButton}>
-              ${generateRandomStockValue()}
-            </button>
+          <button className={globalStyles.stockSymbolButton}>
+            {symbol}
+          </button>
 
-            <Accordion title="Stock Information">
-              <p>This is the content for Stock Information.</p>
-            </Accordion>
+          <button className={globalStyles.stockValueButton}>
+            ${value}
+          </button>
 
-            <button className={globalStyles.whiteBuyButton}>
-              <img src={cart} alt="cart"/>
-              Buy
-            </button>
-            
-          </div>
+          <Accordion title="Stock Information">
+            <p>This is the content for Stock Information.</p>
+          </Accordion>
+
+          <button className={globalStyles.whiteBuyButton}>
+            <img src={cart} alt="cart"/>
+            Buy
+          </button>
+          
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
-export default StockMarket;
+export default StockItem;

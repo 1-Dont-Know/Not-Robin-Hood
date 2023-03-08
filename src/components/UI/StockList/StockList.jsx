7ec6ab41b30dcdc54bfc5d1687be54e9d8482f48
David Bergstrom <davidmbergstrom@gmail.com>
2023-03-07 16:19:34 -0800

    Updated portfolio page to include tabs

---
+++ src/components/UI/StockList/StockList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./StockList.module.scss";
+import { Link } from "react-router-dom";
+
+const StockList = ({ name, symbol, shares, price, avgCost, totalReturn, equity }) => {
+  return (
+    <div className="listItem">
+      <p className={styles.stockName}>{name}</p>
+      <p className={styles.stockSymbol}>{symbol}</p>
+      <p className={styles.stockShares}>{shares}</p>
+      <p className={styles.stockPrice}>{price}</p>
+      <p className={styles.stockAvgCost}>{avgCost}</p>
+      <p className={styles.stockTotalReturn}>{totalReturn}</p>
+      <p className={styles.stockEquity}>{equity}</p>
+    </div>
+  );
+};
+
+export default StockList;
--- src/components/pages/Portfolio/Portfolio.jsx
+++ src/components/pages/Portfolio/Portfolio.jsx
@@ -1,8 +1,7 @@
-import React from "react";
-import Sidebar from "../../UI/Sidebar/Sidebar";
-import TopNav from "../../UI/TopNav/TopNav";
+import React, { useState } from "react";
 import styles from "./Portfolio.module.scss";
 import Hero from "../../UI/Hero/Hero";
+import StockList from "../../UI/StockList/StockList";
 import globalStyles from "../../../styles/main.module.scss";
 import vector from "../../../assets/icons/vector.svg";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
@@ -11,6 +10,21 @@ import { Doughnut } from "react-chartjs-2";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Portfolio = () => {
+
+  const tabFlags = {
+    overview: 1,
+    stocksList: 2,
+  };
+
+  const [activeTab, setActiveTab] = useState(tabFlags.stocksList);
+
+  function handleTabSelect(selectedTab) {
+
+    setActiveTab(curr => {
+      return curr = selectedTab;
+    });
+  }
+
 
   //Fake data for pie chart
   const data = {
@@ -24,6 +38,27 @@ const Portfolio = () => {
       },
     ],
   };
+
+  const stockData = [
+    {
+      name: "Bay Valley Tech",
+      symbol: "BVT",
+      shares: "220",
+      price: "$2.20",
+      avgCost: "$10",
+      totalReturn: "$543.00",
+      equity: "$543.00",
+    },
+    {
+      name: "Bay Valley Tech",
+      symbol: "BVT",
+      shares: "220",
+      price: "$2.20",
+      avgCost: "$10",
+      totalReturn: "$543.00",
+      equity: "$543.00",
+    },
+  ];
 
 
 
@@ -54,45 +89,78 @@ const Portfolio = () => {
       {/* Hero Section */}
       <Hero>
         <div className={styles.tabs}>
-
+          <button className={styles.tabButton} onClick={(e) => { handleTabSelect(tabFlags.overview) }}>Overview</button>
+          <h1 style={{ color: "gray" }}>|</h1>
+          <button className={styles.tabButton} onClick={(e) => { handleTabSelect(tabFlags.stocksList) }}>Stocks List</button>
         </div>
 
 
         <div className={styles.tanSquare}>
-          <div className={styles.details}>
-
-            <section className={styles.totalPortfolio}>
-              <h1 className={styles.totalPortfolioTitle}>Total Portfolio Value</h1>
-              <h1 className={styles.totalPortfolioValue}>$1200.00</h1>
-            </section>
-
-            <section className={styles.stocks}>
-              <h1 className={styles.sectionTitle}>Stocks</h1>
-              <h1 className={styles.sectionPercent}>50%</h1>
-              <h1 className={styles.sectionValue} id={styles.stockValue}>$600</h1>
-            </section>
-
-            <div className={styles.line}></div>
+          {(activeTab === tabFlags.overview) &&
+            <>
+              <div className={styles.details}>
 
 
+                <section className={styles.totalPortfolio}>
+                  <h1 className={styles.totalPortfolioTitle}>Total Portfolio Value</h1>
+                  <h1 className={styles.totalPortfolioValue}>$1200.00</h1>
+                </section>
 
-            <section className={styles.buyingPower}>
-              <h1 className={styles.sectionTitle}>Buying Power</h1>
-              <h1 className={styles.sectionPercent}>50%</h1>
-              <h1 className={styles.sectionValue} id={styles.buyingPowerValue}>$600</h1>
-            </section>
+                <section className={styles.stocks}>
+                  <h1 className={styles.sectionTitle}>Stocks</h1>
+                  <h1 className={styles.sectionPercent}>50%</h1>
+                  <h1 className={styles.sectionValue} id={styles.stockValue}>$600</h1>
+                </section>
 
-            <div className={styles.line}></div>
+                <div className={styles.line}></div>
 
-          </div>
 
-          <div className={styles.doughnutGraph}>
-            <Doughnut
-              data={data}
-              options={options}
-              plugins={[textCenter]}
-            ></Doughnut>
-          </div>
+
+                <section className={styles.buyingPower}>
+                  <h1 className={styles.sectionTitle}>Buying Power</h1>
+                  <h1 className={styles.sectionPercent}>50%</h1>
+                  <h1 className={styles.sectionValue} id={styles.buyingPowerValue}>$600</h1>
+                </section>
+
+                <div className={styles.line}></div>
+
+              </div>
+
+              <div className={styles.doughnutGraph}>
+                <Doughnut
+                  data={data}
+                  options={options}
+                // plugins={[textCenter]}
+                ></Doughnut>
+              </div>
+
+            </>}
+
+          {(activeTab === tabFlags.stocksList) &&
+            <>
+              <section className={styles.transactions}>
+              <StockList
+                        key = {"Name"}
+                        name = {"Name"}
+                        symbol = {"Symbol"}
+                        shares = {"Shares"}
+                        price = {"Price"}
+                        avgCost = {"Average Cost"}
+                        totalReturn = {"Total Return"}
+                        equity = {"Equity"}
+
+                        className={styles.titleBar}
+              />
+                <ul className={styles.transactionsList}>
+                  {stockData.map((item) => {
+                    return (
+                      <></>
+                    );
+                  })}
+                </ul>
+              </section>
+            </>}
+
         </div>
       </Hero>
     </>

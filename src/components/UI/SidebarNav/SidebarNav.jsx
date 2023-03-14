7cb9a95d472381600643250a143a87084c1adbf4
rootfellen <rootfellenua@gmail.com>
2023-03-13 21:55:38 -0700

    fix responsive display of  settings page

--- src/components/UI/SidebarNav/SidebarNav.jsx
+++ src/components/UI/SidebarNav/SidebarNav.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./SidebarNav.module.scss";
 import overview from "../../../assets/icons/overview.svg";
 import markets from "../../../assets/icons/markets.svg";

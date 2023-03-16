796d0d6633e239690955477556da26698a88c0b0
rootfellen <rootfellenua@gmail.com>
2023-03-16 14:21:44 -0700

    add nodemon, change import of express

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
-import express from "express";
-import path from "path";
-
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = 5500;
 
 app.use(express.static(path.join(__dirname, "client")));
+app.get("/express_backend", (req, res) => {
+  //Line 9
+  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" }); //Line 10
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));

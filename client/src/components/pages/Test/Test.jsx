33dd5c07159e014b6c44437fac1df7af7a54d3dd
1-Dont-Know <darshwakdhami@yahoo.com>
2023-03-20 13:47:23 -0700

    testing api

--- client/src/components/pages/Test/Test.jsx
+++ client/src/components/pages/Test/Test.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios"
 
 // const finnhub = require('finnhub');
 
@@ -13,14 +14,41 @@ import React, { useState } from "react";
 
 
 const Test = () =>{
+    const url = "https://finnhub.io/api/v1/search?q=&token=cgal7v9r01qkpvoj1i80cgal7v9r01qkpvoj1i8g";
+    // fetch(url)
+    //     .then(res => res.json())
+    //     .then(data => console.log(data));
 
-    fetch("https://finnhub.io/api/v1/search?q=apple")
+    const [getResults, setResults] = useState(null);
+    const[count, setCount] = useState();
+    // useEffect(() => {
+    //     axios.get(url).then((resp)=> {
+    //         setCount(resp.data.count);
+    //         setResults(resp.data.result);
+    //     })
+    // }, [])
+
+    fetch(url)
         .then(res => res.json())
         .then(data => console.log(data));
 
+    // console.log(count)
+    // console.log(getResults)
+    // fetch("https://finnhub.io/api/v1/stock/candle?symbol=AAPL&resolution=1&from=1631022248&to=1631627048")
+    //     .then(res => res.json())
+    //     .then(data => console.log(data.c));
+
+    // const [priceStock, setPriceStock] = useState(null);
+    // useEffect(() => {
+    //     axios.get("https://finnhub.io/api/v1/stock/candle?symbol=IBM&resolution=D&from=1572651390&to=1575243390")
+    //     .then((resp)=> {
+    //     setPriceStock(resp.data.c);
+    //     })
+    // }, [])
     return(
         <div> 
             <h1>Helloi</h1>
+            {/* {priceStock} */}
         </div>
     )
 }

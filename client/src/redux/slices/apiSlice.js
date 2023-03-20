c12ba86c5b7d39637e7e929022c2f95889c136b3
rootfellen <rootfellenua@gmail.com>
2023-03-20 14:42:39 -0700

    remove key from api

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,14 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // api link
-const stocksSource =
-  "https://finnhub.io/api/v1/search?q=apple&token=cgal7v9r01qkpvoj1i80cgal7v9r01qkpvoj1i8g";
 
 export const stocksApi = createApi({
   // name
   reducerPath: "stocksApi",
   //   source from where to fetch data from
-  baseQuery: fetchBaseQuery({ baseUrl: stocksSource }),
+  baseQuery: fetchBaseQuery({ baseUrl: "stocksSource" }),
 
   // list of queries
   endpoints: (builder) => ({

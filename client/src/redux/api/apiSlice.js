41738a47ca9adfe264a809e6f6c6d6e261790caf
rootfellen <rootfellenua@gmail.com>
2023-03-20 18:10:56 -0700

    add data objec to server

---
+++ client/src/redux/api/apiSlice.js
@@ -0,0 +1,7 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5500" }),
+  endpoints: (builder) => ({}),
+});
--- server/server.js
+++ server/server.js
@@ -3,8 +3,48 @@ const path = require("path");
 const app = express();
 const port = 5500;
 
+const data = {
+  users: [
+    {
+      id: 1,
+      name: "Jack",
+    },
+    {
+      id: 2,
+      name: "John",
+    },
+    {
+      id: 3,
+      name: "Mike",
+    },
+    {
+      name: "test",
+      id: 4,
+    },
+    {
+      name: "ddd",
+      id: 5,
+    },
+    {
+      name: "gggg",
+      id: 6,
+    },
+    {
+      name: "dddd",
+      id: 7,
+    },
+    {
+      name: "!!!!",
+      id: 8,
+    },
+    {
+      name: "aaaaa",
+      id: 9,
+    },
+  ],
+};
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(data);
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));

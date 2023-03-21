cc44cc4891e6c829540608ee84159a795d4ba855
rootfellen <rootfellenua@gmail.com>
2023-03-21 14:02:31 -0700

    connect planetscale database

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-const port = 5500;
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+import mysql from "mysql2";
 
-const data = {
-  users: [
+// FOR WORKING WITH ENVIRONMENT VARIABLES
+dotenv.config();
+
+// CONNECTION TO OUR DATABASE
+const connection = await mysql.createConnection(process.env.DATABASE_URL);
+console.log("Connected to PlanetScale!");
+connection.end();
+
+const port = process.env.PORT || 5500;
+
+console.log(port);
+
+// FRAMEWORK WE WILL USE FOR BUILDING OUR API
+const app = express();
+
+// TEMPORARY DATA TO SHOW
+const user = {
+  data: [
     {
       id: 1,
       name: "Jack",
@@ -13,38 +29,13 @@ const data = {
       id: 2,
       name: "John",
     },
-    {
-      id: 3,
-      name: "Mike",
-    },
-    {
-      name: "test",
-      id: 4,
-    },
-    {
-      name: "ddd",
-      id: 5,
-    },
-    {
-      name: "gggg",
-      id: 6,
-    },
-    {
-      name: "dddd",
-      id: 7,
-    },
-    {
-      name: "!!!!",
-      id: 8,
-    },
-    {
-      name: "aaaaa",
-      id: 9,
-    },
   ],
 };
+
+// DEFAULT ENDPOINT TO GET TEMPORARY DATA
 app.get("/", (req, res) => {
-  res.send(data);
+  res.send(user);
 });
 
+// TO CHECK IF OUR APP US RUNNING AND ON WHICH PORT
 app.listen(port, () => console.log(`Server is running on port ${port}`));

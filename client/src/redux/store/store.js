7660cfc04b95da3b88ec784c2a5a3f7161386e03
1-Dont-Know <darshwakdhami@yahoo.com>
2023-03-21 15:24:32 -0700

    added slices for setting page

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -1,8 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import balanceReducer from "../slices/balanceSlice";
+import darkModeReducer from "../slices/darkModeSlice";
+import changePasswordReducer from "../slices/changePasswordSlice";
+import changeNameReducer from "../slices/changeNameSlice";
 
 export const store = configureStore({
   reducer: {
     balance: balanceReducer,
+  },
+  reducer: {
+    darkMode: darkModeReducer,
+  },
+  reducer: {
+    changePassword: changePasswordReducer,
+  },
+  reducer: {
+    changeName: changeNameReducer,
   },
 });

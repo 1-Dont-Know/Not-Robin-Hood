2daa03f2f648de0d18417c739e1d637abe2db0c4
Hoang Nguyen <hoangisnot@gmail.com>
2023-03-21 15:13:20 -0700

    created slice for stockItem on market page

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import balanceReducer from "../slices/balanceSlice";
+import StocksItemReducer from "../slices/stockItemSlice"
 import { stocksApi } from "../slices/apiSlice";
-
 
 export const store = configureStore({
   reducer: {
     [stocksApi.reducerPath]: stocksApi.reducer,
     balance: balanceReducer,
+    stocksItem: StocksItemReducer,
   },
 
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(stocksApi.middleware),

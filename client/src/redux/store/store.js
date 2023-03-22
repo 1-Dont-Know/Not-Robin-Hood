1fe40e8c7129f9aa84e5abd44d0ccb23ca682a03
IDK <61032214+1-Dont-Know@users.noreply.github.com>
2023-03-22 14:42:18 -0700

    fixed

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -1,20 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
-import balanceReducer from "../slices/balanceSlice";
-import StocksItemReducer from "../slices/stockItemSlice"
+
+import StocksItemReducer from "../slices/stockItemSlice";
 import { stocksApi } from "../slices/apiSlice";
 import darkModeReducer from "../slices/darkModeSlice";
 import changePasswordReducer from "../slices/changePasswordSlice";
 import changeNameReducer from "../slices/changeNameSlice";
+import { balanceApi } from "../slices/userApiSlice";
 
 export const store = configureStore({
   reducer: {
     [stocksApi.reducerPath]: stocksApi.reducer,
-    balance: balanceReducer,
+    [balanceApi.reducerPath]: balanceApi.reducer,
     stocksItem: StocksItemReducer,
     darkMode: darkModeReducer,
     changePassword: changePasswordReducer,
     changeName: changeNameReducer,
   },
   
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(stocksApi.middleware),
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware().concat(stocksApi.middleware),
 });

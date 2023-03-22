5dcb80f0764b53bf13f284989c214b7c0887a8ad
IDK <61032214+1-Dont-Know@users.noreply.github.com>
2023-03-22 14:51:14 -0700

    fixxed

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import path from "path";
 import dotenv from "dotenv";
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
+import cors from "cors";
 
 // FOR WORKING WITH ENVIRONMENT VARIABLES
 dotenv.config();
@@ -9,32 +10,27 @@ dotenv.config();
 // CONNECTION TO OUR DATABASE
 const connection = await mysql.createConnection(process.env.DATABASE_URL);
 console.log("Connected to PlanetScale!");
-connection.end();
 
 const port = process.env.PORT || 5500;
-
-console.log(port);
 
 // FRAMEWORK WE WILL USE FOR BUILDING OUR API
 const app = express();
 
-// TEMPORARY DATA TO SHOW
-const user = {
-  data: [
-    {
-      id: 1,
-      name: "Jack",
-    },
-    {
-      id: 2,
-      name: "John",
-    },
-  ],
-};
+app.use(cors());
 
-// DEFAULT ENDPOINT TO GET TEMPORARY DATA
 app.get("/", (req, res) => {
-  res.send(user);
+  res.json("WELCOME TO HOBIN ROOD APP SERVER");
+});
+
+// ENDPOINT TO GET BALANCE
+app.get("/balance", async (req, res) => {
+  try {
+    const query = "SELECT Balance FROM User;";
+    const [rows] = await connection.query(query);
+    res.json(rows);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // TO CHECK IF OUR APP US RUNNING AND ON WHICH PORT

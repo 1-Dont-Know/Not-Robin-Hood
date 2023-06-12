a8a0565e84af50ecb96d06887e8ca23632ae084f
David Bergstrom <davidmbergstrom@gmail.com>
2023-06-12 10:03:14 -0700

    Added ability to click featured stocks to view stock info

--- client/src/components/UI/FeaturedStock/FeaturedStock.jsx
+++ client/src/components/UI/FeaturedStock/FeaturedStock.jsx
@@ -1,47 +1,58 @@
-import { React, useEffect, useState} from "react";
+import {React, useEffect, useState} from "react";
 import styles from "./FeaturedStock.module.scss";
 import featured from "../../../assets/icons/featured.svg";
 import up from "../../../assets/icons/up.svg";
-import { useGetPriceQuery } from "../../../redux/slices/api/finnhubApiSlice";
+import {useGetPriceQuery} from "../../../redux/slices/api/finnhubApiSlice";
+import {Link} from "react-router-dom";
 
 const useStockPrice = (symbol) => {
-  return useGetPriceQuery(symbol);
+	return useGetPriceQuery(symbol);
 };
 
-const FeaturedStock = ({ symbol, name }) => {
-  const { data, error, isLoading } = useStockPrice(symbol);
-  const [status, setStatus] = useState(); //State used to track if there is current gain or loss for stock
-  
-  useEffect(() => {
-    if (data) {
-      setStatus(() => {
-        return (parseFloat(data.d) > 0) ? "up" : "down"; 
-      });
-      // console.log(data);
-      // console.log(typeof data.d)
-    }
-  }, [data]);
+const FeaturedStock = ({symbol, name}) => {
+	const {data, error, isLoading} = useStockPrice(symbol);
+	const [status, setStatus] = useState(); //State used to track if there is current gain or loss for stock
 
-  return (
-    data && (<div className={status === "up" ? styles.positive : styles.negative}>
-      <div className={styles.stockInformation}>
-        <div className={styles.stockIcon}>
-          <img src={featured} alt="stock-icon" />
-        </div>
-        <div className={styles.stockName}>
-          <h1>{symbol}</h1>
-          <h4>{name}</h4>
-        </div>
-      </div>
-      <div className={styles.stockResults}>
-        <p>{`%${data.dp}`}</p>
-        <span>
-          <img src={up} alt="up" />
-        </span>
-      </div>
-      <div className={styles.stockPrice}>{(parseFloat(data.d) > 0) ? `+$${data.d}` : `-$${data.d.toString().slice(1)}` }</div>
-    </div>
-    ));
+	useEffect(() => {
+		if (data) {
+			setStatus(() => {
+				return parseFloat(data.d) > 0 ? "up" : "down";
+			});
+			// console.log(data);
+			// console.log(typeof data.d)
+		}
+	}, [data]);
+
+	return (
+		data && (
+			<Link
+				to={{
+					pathname: "/stock-viewer",
+					search: `?symbol=${symbol}&description=${name.replace(/\s+/g, "+")}`,
+				}}>
+				<div className={status === "up" ? styles.positive : styles.negative}>
+					<div className={styles.stockInformation}>
+						<div className={styles.stockIcon}>
+							<img src={featured} alt="stock-icon" />
+						</div>
+						<div className={styles.stockName}>
+							<h1>{symbol}</h1>
+							<h4>{name}</h4>
+						</div>
+					</div>
+					<div className={styles.stockResults}>
+						<p>{`%${data.dp}`}</p>
+						<span>
+							<img src={up} alt="up" />
+						</span>
+					</div>
+					<div className={styles.stockPrice}>
+						{parseFloat(data.d) > 0 ? `+$${data.d}` : `-$${data.d.toString().slice(1)}`}
+					</div>
+				</div>
+			</Link>
+		)
+	);
 };
 
 export default FeaturedStock;

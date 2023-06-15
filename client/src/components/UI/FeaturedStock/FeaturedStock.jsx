762d14fcdf898695f9dae8601f7e87190e180623
Aveneil Lucena <neillucena97@gmail.com>
2023-06-15 14:33:37 -0700

    dark mode for featured stocks on overview page

--- client/src/components/UI/FeaturedStock/FeaturedStock.jsx
+++ client/src/components/UI/FeaturedStock/FeaturedStock.jsx
@@ -3,6 +3,9 @@ import styles from "./FeaturedStock.module.scss";
 import featured from "../../../assets/icons/featured.svg";
 import up from "../../../assets/icons/up.svg";
 import { useGetPriceQuery } from "../../../redux/slices/api/finnhubApiSlice";
+//Dark Mode
+import { useSelector } from 'react-redux';
+import { selectDarkMode, toggleTheme } from './../../../redux/slices/darkModeSlice';
 
 const useStockPrice = (symbol) => {
   return useGetPriceQuery(symbol);
@@ -12,6 +15,12 @@ const FeaturedStock = ({ symbol, name }) => {
   const { data, error, isLoading } = useStockPrice(symbol);
   const [status, setStatus] = useState(); //State used to track if there is current gain or loss for stock
   
+    {/* Dark Mode Theme*/}
+    const darkModeTheme = useSelector(selectDarkMode);
+    // When Settings page is rendered, we will set our localstorage "darkMode": false by default;
+    useEffect(() => {localStorage.setItem("darkMode", darkModeTheme);}, [darkModeTheme]);
+    {/* End Dark Mode Theme*/}
+
   useEffect(() => {
     if (data) {
       setStatus(() => {
@@ -23,23 +32,24 @@ const FeaturedStock = ({ symbol, name }) => {
   }, [data]);
 
   return (
-    data && (<div className={status === "up" ? styles.positive : styles.negative}>
+    data && (<div className={`${status === "up" ? styles.positive : styles.negative} 
+        ${darkModeTheme ? styles["dark-mode"] : ""}`}>
       <div className={styles.stockInformation}>
-        <div className={styles.stockIcon}>
+        <div className={`${styles.stockIcon} ${darkModeTheme ? styles["dark-mode"] : ""}`}>
           <img src={featured} alt="stock-icon" />
         </div>
-        <div className={styles.stockName}>
+        <div className={`${styles.stockName} ${darkModeTheme ? styles["dark-mode"] : ""}`}>
           <h1>{symbol}</h1>
           <h4>{name}</h4>
         </div>
       </div>
-      <div className={styles.stockResults}>
+      <div className={`${styles.stockResults} ${darkModeTheme ? styles["dark-mode"] : ""}`}>
         <p>{`%${data.dp}`}</p>
         <span>
           <img src={up} alt="up" />
         </span>
       </div>
-      <div className={styles.stockPrice}>{(parseFloat(data.d) > 0) ? `+$${data.d}` : `-$${data.d.toString().slice(1)}` }</div>
+      <div className={`${styles.stockPrice} ${darkModeTheme ? styles["dark-mode"] : ""}`}>{(parseFloat(data.d) > 0) ? `+$${data.d}` : `-$${data.d.toString().slice(1)}` }</div>
     </div>
     ));
 };

145ae649b2b489d7a369b80e0bd7fa47ad34435b
Aveneil Lucena <neillucena97@gmail.com>
2023-06-22 14:58:01 -0700

    Overview page graph dark mode styles

--- client/src/components/UI/Graph/Graph.jsx
+++ client/src/components/UI/Graph/Graph.jsx
@@ -1,29 +1,66 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Graph.module.scss";
 import { Chart as ChartJS } from "chart.js/auto";
 import { Line } from "react-chartjs-2";
+import { useSelector } from 'react-redux';
+import { selectDarkMode } from './../../../redux/slices/darkModeSlice';
 
 // Graph component
 const Graph = ({ chartData }) => {
-  const options = {
-    maintainAspectRatio: false,
-    responsive: true,
-    plugins: {
-      title: {
-        display: true,
-        // text: "GAINS!",
+    /* Dark Mode Theme*/
+    const darkModeTheme = useSelector(selectDarkMode);
+    useEffect(() => {localStorage.setItem("darkMode", darkModeTheme);}, [darkModeTheme]);
+
+    const options = {
+      maintainAspectRatio: false,
+      responsive: true,
+      plugins: {
+        tooltip: {
+          callbacks: {
+            label: function (context) {
+              const value = context.parsed.y;
+              return ` $ ${value.toFixed(2)}`;
+            },
+          },
+        },
+        title: {
+          display: true,
+          text: "GAINS!",
+        },
+        legend: {
+          display: false,
+        },
       },
-      legend: {
-        display: false,
+      //Edit the X+Y Axis Colors
+      scales: {
+        x: {
+          grid: {
+            color: darkModeTheme ? "#345345" : "lightgrey",
+          },
+          ticks: {
+            color: darkModeTheme ? "lightgrey" : "black",
+          },
+        },
+        y: {
+          grid: {
+            color: darkModeTheme ? "#345345" : "lightgrey",
+          },
+          ticks: {
+            color: darkModeTheme ? "lightgrey" : "black",
+          }
+        }
       },
-    },
+  
+      
+    };
+  
+    return (
+      <div className={`${styles.container} ${darkModeTheme ? styles["dark-mode"] : ""}`}>
+        <Line data={chartData} options={options} />
+      </div>
+    );
   };
-
-  return (
-    <div className={styles.container}>
-      <Line data={chartData} options={options} />
-    </div>
-  );
-};
-
-export default Graph;
+  
+  export default Graph;
+  
+  

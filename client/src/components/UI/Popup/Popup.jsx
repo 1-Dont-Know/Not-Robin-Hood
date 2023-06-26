3ff71927f4f646f7c1fd2f5af3882e9b94cb9600
Aveneil Lucena <neillucena97@gmail.com>
2023-06-26 03:49:58 -0700

    notifications popup dark mode styles

--- client/src/components/UI/Popup/Popup.jsx
+++ client/src/components/UI/Popup/Popup.jsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Popup.module.scss";
+import { useSelector } from 'react-redux';
+import { selectDarkMode } from './../../../redux/slices/darkModeSlice';
 
 const Popup = ({ name, toggle, children }) => {
+  const darkModeTheme = useSelector(selectDarkMode);
+  // When Settings page is rendered, we will set our localstorage "darkMode": false by default;
+  useEffect(() => {localStorage.setItem("darkMode", darkModeTheme);}, [darkModeTheme]);
+  
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${darkModeTheme ? styles["dark-mode"] : ""}`}>
       <div
-        className={
-          name === "notifications"
-            ? styles.popup
-            : name === "sellStock"
-            ? styles.sellStock
-            : styles.popUpFunds
-        }
+        className={`${name === "notifications" ? styles.popup :
+        name === "sellStock" ? styles.sellStock : styles.popUpFunds}
+        ${darkModeTheme ? styles["dark-mode"] : ""}`}
       >
         <button
           onClick={toggle}
